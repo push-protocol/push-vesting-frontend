011c137b3d62feefcba701574c3d9f2a4fc5877f
Harsh Rajat <ProjectHydro@3602652.no-reply.drupal.org>
2021-04-13 06:04:56 +0530

    Updates

---
+++ precache-manifest.fb87f36b0c2de72811d60789780792d7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f675151f90f45018d75d67bd128b5663",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6865436373f85ea8e6d9",
+    "url": "/static/css/2.3a221eca.chunk.css"
+  },
+  {
+    "revision": "c8c4fad1fd1b33147465",
+    "url": "/static/css/main.925e3c97.chunk.css"
+  },
+  {
+    "revision": "6865436373f85ea8e6d9",
+    "url": "/static/js/2.678d61f3.chunk.js"
+  },
+  {
+    "revision": "ff0e22e3f0a21c20464e211953ea98ae",
+    "url": "/static/js/2.678d61f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ed0250f2fb5d2f542c0",
+    "url": "/static/js/3.aaac04fd.chunk.js"
+  },
+  {
+    "revision": "d828d064fd611903189974745a0859f2",
+    "url": "/static/js/3.aaac04fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8c4fad1fd1b33147465",
+    "url": "/static/js/main.c27ae878.chunk.js"
+  },
+  {
+    "revision": "b75956ea4e52afd07f12",
+    "url": "/static/js/runtime-main.8872e02f.js"
+  },
+  {
+    "revision": "9e0373c2b4410c49439dfd822c5fd16e",
+    "url": "/static/media/spinner.9e0373c2.gif"
+  }
+]);
